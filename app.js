3eca1579a01105a661937ec4df6cd5da1a58ebcf
Mustfa1122 <145039363+Mustfa1122@users.noreply.github.com>
2023-11-23 07:32:48 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var arr = ["raza","hassan","naveed","shehzad","ali","moiz"]
+var userName = "raza"
+for(var i = 0 ; i < arr.length; i++){
+    if (userName == arr[i]){
+        alert("this name is match")
+    }
+        else{
+            alert("this name is not match")
+        }
+
+}
